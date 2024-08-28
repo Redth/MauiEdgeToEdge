c8ea855d9ddbd5b9b1fa0fbc8a9e909c15bc6144
redth <jondick@gmail.com>
2024-08-28 15:33:21 -0400

    Fix android status bar inset

--- MauiEdgeToEdge/MauiProgram.cs
+++ MauiEdgeToEdge/MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Android.App;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.AppBar;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Platform;
 
 namespace MauiEdgeToEdge
 {
@@ -15,11 +20,28 @@ namespace MauiEdgeToEdge
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            Microsoft.Maui.Handlers.ToolbarHandler.Mapper.AppendToMapping("FixInsets", (handler, view)=>
+            {
+                var parentView = view.Parent?.Handler?.PlatformView;
+
+                if (parentView is Activity activity)
+                {
+                    activity?.FindViewById<AppBarLayout>(Resource.Id.navigationlayout_appbar)
+                        ?.SetFitsSystemWindows(true);
+                }
+                else if (parentView is Android.Views.View androidView)
+                {
+                    androidView?.FindViewById<AppBarLayout>(Resource.Id.navigationlayout_appbar)
+                        ?.SetFitsSystemWindows(true);
+                }
+            });
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
         }
+
+      
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,26 @@ If we enable Edge to Edge on Android in a MAUI app (built against, targeting, an
 ![image](https://github.com/user-attachments/assets/7f3d46cf-bd24-4e7c-b383-f7a117db5b10)
 
 > The 'EdgeToEdge' class was ported from Kotlin code until we reference newer AndroidX packages which contain this functionality
+
+
+### Fixing the MaterialToolbar on NavigationPage
+
+It looks like we use `MaterialToolbar` nested inside an `AppBarLayout`.  The `AppBarLayout` needs to be set to fit system windows in order to have the correct padding to cause content to not layout beneath the status bar:
+
+```csharp
+Microsoft.Maui.Handlers.ToolbarHandler.Mapper.AppendToMapping("FixInsets", (handler, view)=>
+{
+    var parentView = view.Parent?.Handler?.PlatformView;
+
+    if (parentView is Activity activity)
+    {
+        activity?.FindViewById<AppBarLayout>(Resource.Id.navigationlayout_appbar)
+            ?.SetFitsSystemWindows(true);
+    }
+    else if (parentView is Android.Views.View androidView)
+    {
+        androidView?.FindViewById<AppBarLayout>(Resource.Id.navigationlayout_appbar)
+            ?.SetFitsSystemWindows(true);
+    }
+});
+```
